5afb13674c7df6def263816ccb9391c7ebe4eee0
JhosefMatheus <jhosef.mat123@hotmail.com>
2022-10-09 04:06:00 -0300

    Finalizando estilo da tela de registro e iniciando backend da tela de registro

--- backend/DBManager.py
+++ backend/DBManager.py
@@ -1,4 +1,5 @@
 from app.models import *
+from app import db
 
 
 class DBManager:
@@ -9,3 +10,23 @@ class DBManager:
         user = User.query.filter_by(userLogin=login, userPassword=password).first()
 
         return user
+
+    
+    def get_user_by_login(self, login):
+        user = User.query.filter_by(userLogin=login).first()
+
+        return user
+
+
+    def get_user_by_password(self, password):
+        user = User.query.filter_by(userPassword=password).first()
+
+        return user
+
+
+    def insert_user(self, first_name, last_name, login, password):
+        user = User(first_name, last_name, login, password)
+
+        db.session.add(user)
+
+        db.session.commit()
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -21,3 +21,29 @@ def login_verify():
         return make_response(jsonify({"message": "login in succefull"}), 200)
 
     return make_response(jsonify({"message": "login or password doesn't match"}), 401)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    req = request.get_json()
+
+    f_name = req["firstName"]
+    l_name = req["lastName"]
+    login = req["login"]
+    password = req["password"]
+
+    hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+    login_verify = dbManager.get_user_by_login(login)
+
+    if not login_verify:
+        password_verify = dbManager.get_user_by_password(hashed_password)
+
+        if not password_verify:
+            dbManager.insert_user(f_name, l_name, login, hashed_password)
+
+            return make_response(jsonify({"message": "Registrado com sucesso!"}), 200)
+
+        return make_response(jsonify({"message": "Senha indisponível"}), 403)
+
+    return make_response(jsonify({"message": "Login indisponível"}), 403)
