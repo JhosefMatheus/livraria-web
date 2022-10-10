9a0dda9cd53364c442623a1ee00c63edccb047c2
JhosefMatheus <jhosef.mat123@hotmail.com>
2022-10-10 01:05:44 -0300

    Adicionandao side bar, bar e campo de pesquisa à página de livros

--- frontend/js/books.js
+++ frontend/js/books.js
@@ -1,3 +1,10 @@
+const menuButton = document.getElementById("menu-button");
+const sideBar = document.getElementById("side-bar");
+let menuButtonFlag = "closed";
+const booksOption = document.getElementById("books-option");
+const booksIcon = document.getElementById("books-icon");
+const booksText = document.getElementById("books-text");
+
 window.onload = async () => {
     const token = localStorage.getItem("token");
 
@@ -23,6 +30,18 @@ window.onload = async () => {
 
         const jsonBooks = await booksResponse.json()
 
-        console.log(jsonBooks);
+        booksOption.style.backgroundColor = "rgba(255, 255, 255, 0.08)";
+        booksIcon.style.fill = "rgb(16, 185, 129)";
+        booksText.style.color = "rgb(16, 185, 129)";
     }
 }
+
+menuButton.addEventListener("click", () => {
+    if (menuButtonFlag === "closed") {
+        sideBar.style.display = "block";
+        menuButtonFlag = "opened";
+    } else {
+        sideBar.style.display = "none";
+        menuButtonFlag = "closed";
+    }
+});
